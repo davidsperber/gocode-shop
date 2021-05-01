e03dd9a6d63cb230f5697b66305bfce71605a91a
david sperber <dcsperber@gmail.com>
2021-05-02 00:54:16 +0300

    e2

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ import './App.css';
 import './main.css'
 // import MyButtom from './MyButton';
 import Header from './Header';
-import Prodcts1 from './Prodcts'
-import Prodct from './Prodcts'
+// import Prodcts1 from './Prodcts'
+// import Prodct from './Prodcts'
 // import Products2 from './Products2'
 import Ex2 from './ex2'
-// import 
+
 function App() {
 
     return (
         <div>
         <Header />
-        <Prodcts1/>
-        {/* <Ex2/> */}
+        {/* <Prodcts1/> */}
+        <Ex2 />
         </div>
     );
 
--- src/Products2.js
+++ src/Products2.js
@@ -1,15 +1,15 @@
 import './App.css';
-import './main.css'
-function Products2({category,image,price,description,title}) { 
+import './main.css';
+import './style.css';
+function Products2({ category, image, price, description, title }) {
     return (
 
         <div className="product-card">
             <div className="product-image">
-                {/* <img src="https://cdn.shopify.com/s/files/1/0938/8938/products/10231100205_1_1315x1800_300_CMYK_1024x1024.jpeg?v=1445623369"/> */}
-                <img src={image} alt={description} title={title}/>
             </div>
             <div className="product-info">
-                <h5>{category}</h5>
+                <img src={image} alt={description} />
+                <h4>{title}</h4>
                 <h6>{price}</h6>
             </div>
         </div>
